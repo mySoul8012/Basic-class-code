3777aff29b10b9ea9654519028619564a201d9c2
melovemingming <mingming@mingming.email>
2019-12-04 17:06:49 +0800

    day-04

---
+++ day-04/src/com/ming/Demo1.java
@@ -0,0 +1,10 @@
+package com.ming;
+
+public class Demo1 {
+    public static void main(String[] args){
+        int[] number = {1, 2, 3, 4, 5};
+        for(int i = 0; i < number.length; i++){
+            System.out.println(number[i]);
+        }
+    }
+}
