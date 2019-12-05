a16904e1dfdc2d83e9acfe5c0fe00a6709dff2d9
melovemingming <mingming@mingming.email>
2019-12-05 12:55:39 +0800

    day-05

--- day-05/src/com/ming/Demo3.java
+++ day-05/src/com/ming/Demo3.java
@@ -12,6 +12,11 @@ public class Demo3 {
         print(m, n);
     }
     public static void print(int m, int n){
-
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print("@");
+            }
+            System.out.println();
+        }
     }
 }
