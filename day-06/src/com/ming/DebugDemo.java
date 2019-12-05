6c82911f86f388780264a35d1e5f237a1196a4c2
melovemingming <mingming@mingming.email>
2019-12-05 10:17:12 +0800

    day-06

---
+++ day-06/src/com/ming/DebugDemo.java
@@ -0,0 +1,10 @@
+package com.ming;
+
+public class DebugDemo {
+    public static void main(String[] args){
+        int i = 10;
+        int j = 20;
+        int sum = i + j;
+        System.out.println(sum);
+    }
+}
