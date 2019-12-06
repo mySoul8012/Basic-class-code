2526b90eb00d08771a65f92c0055eaeadf5fbdf5
melovemingming <mingming@mingming.email>
2019-12-06 17:30:42 +0800

    day-07

--- day-07/src/com/ming/demo01/Student.java
+++ day-07/src/com/ming/demo01/Student.java
@@ -19,4 +19,9 @@ public class Student {
     public void setAge(int age) {
         this.age = age;
     }
+
+    @Override
+    public String toString() {
+        return this.age + this.name;
+    }
 }
