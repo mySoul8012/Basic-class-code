c79db25b5522edeb541f3399879a621184e3763a
melovemingming <mingming@mingming.email>
2019-12-06 18:16:22 +0800

    day-07

---
+++ day-07/src/com/ming/demo2/Demo2.java
@@ -0,0 +1,24 @@
+package com.ming.demo2;
+
+import java.util.Random;
+
+public class Demo2 {
+    public static void main(String[] args){
+        int[] numberInt = new int[10];
+        // 进行数组填充
+        Random random = new Random();
+        for(int i = 0; i < numberInt.length; i++){
+            int randomInt = random.nextInt(100) + 101;
+            numberInt[i] = randomInt;
+        }
+        // 计算奇数索引的和
+        int sum = 0;
+        for(int i = 0; i < numberInt.length; i++){
+            // 判断是否是奇数
+            if()
+        }
+
+    }
+
+
+}
