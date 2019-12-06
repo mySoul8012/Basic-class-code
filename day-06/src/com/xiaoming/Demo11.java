7bfa90cf68e77199e37ece252c5b1ea4129c14e1
melovemingming <mingming@mingming.email>
2019-12-06 16:31:36 +0800

    day-06

--- day-06/src/com/xiaoming/Demo11.java
+++ day-06/src/com/xiaoming/Demo11.java
@@ -1,4 +1,18 @@
 package com.xiaoming;
 
 public class Demo11 {
+    public static void main(String[] args){
+        char[] chArray = {'a', 'b', '7', 'A', '0', '你', '好', '9'};
+        int sum = 0;
+        for(int i = 0; i < chArray.length; i++){
+            if(!Character.isDigit(chArray[i])){
+                sum++;
+            }
+        }
+        System.out.print(sum);
+    }
+
+
+
+
 }
---
+++ day-06/src/com/xiaoming/Demo12.java
@@ -0,0 +1,10 @@
+package com.xiaoming;
+
+public class Demo12 {
+    public static void main(String[] args){
+
+    }
+
+    // 生成10个长度的数组
+    public static int[]
+}
