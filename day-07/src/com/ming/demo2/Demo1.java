e706da8822a71b4518f0f5805da0a892b3de5010
melovemingming <mingming@mingming.email>
2019-12-07 08:05:01 +0800

    day-07

--- day-07/src/com/ming/demo2/Demo1.java
+++ day-07/src/com/ming/demo2/Demo1.java
@@ -1,19 +1,4 @@
 package com.ming.demo2;
 
-import com.ming.demo01.Student;
-
 public class Demo1 {
-    public static void main(String[] args){
-        Student student = new Student();
-        student.setName("ming");
-        student.setAge(22);
-
-        System.out.println(student.toString());
-
-        student.setName("fregeg");
-        student.setAge(546);
-        System.out.println(student.toString());
-
-
-    }
 }
