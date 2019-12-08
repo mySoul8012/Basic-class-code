e4f1a4d91cac08aada49304a586dfcad4c4cda95
melovemingming <mingming@mingming.email>
2019-12-09 00:30:26 +0800

    day-08

---
+++ day-08/src/com/xiaoming2/Tets09.java
@@ -0,0 +1,23 @@
+package com.xiaoming2;
+
+public class Tets09 {
+    public static void main(String[] args){
+        // 统计一文本中java出现的次数
+        String text = "Java语言是面向对象的，Java语言是健壮的，Java语言是安全的，Java是高性能的，Java语言是跨平台的";
+        // 进行文本的统计
+        // 进行java计数
+        int count = 0;
+        // 记录当前的Java索引
+        int index = 0;
+        index = text.indexOf("Java");
+        while(text.length() >= 0){
+            index = text.indexOf("Java");
+            if(index < 0){
+                break;
+            }
+            text = text.substring(index + 4);
+            count++;
+        }
+        System.out.println(count);
+    }
+}
